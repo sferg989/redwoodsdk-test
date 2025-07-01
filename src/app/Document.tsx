c5077f0a88d46cda02e1073663d311ab715fa03e
Stephen Ferguson <sferg989@gmail.com>
2025-07-01 17:03:43 -0500

    init

---
+++ src/app/Document.tsx
@@ -0,0 +1,19 @@
+import styles from "./styles.css?url";
+
+export const Document: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => (
+  <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <link rel="stylesheet" href={styles} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>@redwoodjs/starter-standard</title>
+      <link rel="modulepreload" href="/src/client.tsx" />
+    </head>
+    <body>
+      <div id="root">{children}</div>
+      <script>import("/src/client.tsx")</script>
+    </body>
+  </html>
+);
