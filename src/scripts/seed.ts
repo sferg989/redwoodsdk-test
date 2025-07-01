d42e961c82e5045e9aa4d5efd3b2510304d6f853
Stephen Ferguson <sferg989@gmail.com>
2025-07-01 17:18:37 -0500

    i think it worked

--- src/scripts/seed.ts
+++ src/scripts/seed.ts
@@ -1,20 +1,27 @@
 import { defineScript } from "rwsdk/worker";
-import { db, setupDb } from "@/db";
-import { env } from "cloudflare:workers";
+import { db, setupDb } from "../db";
 
-export default defineScript(async () => {
-  await setupDb(env);
+export default defineScript(async ({ env }) => {
+  setupDb(env);
 
   await db.$executeRawUnsafe(`\
+    DELETE FROM Application;
+    DELETE FROM ApplicationStatus;
+    DELETE FROM Contact;
+    DELETE FROM Company;
+    DELETE FROM Credential;
     DELETE FROM User;
     DELETE FROM sqlite_sequence;
   `);
 
-  await db.user.create({
-    data: {
-      id: "1",
-      username: "testuser",
-    },
+  await db.applicationStatus.createMany({
+    data: [
+      { id: 1, status: "New" },
+      { id: 2, status: "Applied" },
+      { id: 3, status: "Interview" },
+      { id: 4, status: "Rejected" },
+      { id: 5, status: "Offer" },
+    ],
   });
 
   console.log("🌱 Finished seeding");
